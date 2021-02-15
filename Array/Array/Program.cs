6b73dee4554e415def9447a9ccbed51e1d7cfbea
Mushfiqur Abir <mushfiqur.abir@gmail.com>
2021-02-15 12:00:59 +0600

    Array 2nd try

--- Array/Array/Program.cs
+++ Array/Array/Program.cs
@@ -31,14 +31,17 @@ namespace Array
 
             Console.WriteLine();
 
-           //Console.WriteLine("Copy Array");
+           Console.WriteLine("Copy Array");
 
-            /*int[] arr2 = new int[] { 1, 2, 3, 4, 5, 6, 7, 8 };
-            for(int i=0;i< arr2.Length;i++)
+            int[] arr2 = new int[] { 1, 2, 3, 4, 5, 6, 7, 8 };
+            int[] arr3 = new int[] {8,9,12,15,17,89};
+            for(int i=0;i < arr2.Length;i++)
             {
-                int arr3= arr2[i];
-                Console.WriteLine(arr3[i]);
-            }*/
+               arr3= arr2;
+                Console.WriteLine(arr3);
+            }
+
+            Console.WriteLine();
 
             Console.WriteLine("Duplicate finding Array");
 
@@ -52,6 +55,21 @@ namespace Array
                     if (arr4[i] == arr4[j])
                         Console.WriteLine(arr4[j]);
                 }
+            }
+            
+            Console.WriteLine();
+
+            Console.WriteLine("Unique Array");
+
+            int[] arr5 = { 1, 2, 3, 2, 1, 4, 5, 6, 5 };
+            for(int i = 0; i< arr5.Length; i++)
+            {
+                int j;
+                for (j = 0; j < i; j++)
+                    if (arr5[i] == arr5[j])
+                        break;
+                if (i == j)
+                    Console.WriteLine(arr5[i] + " ");
             }
         }
     }
